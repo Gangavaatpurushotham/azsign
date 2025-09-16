fa89e8e09ff4b688eb797475581e4c0927da50de
Your Name <your.email@example.com>
2025-09-16 12:19:42 +0530

    f1

---
+++ blog-app/src/F1.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Route , Routes} from 'react-router-dom';
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
+
+function F1(){
+    return(<Router>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+    </Router>)
+}
+export default F1;
